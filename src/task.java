3bc96d0c174d224ae714d1f05628adb4cddf0899
zeynepsteel <lilyfoster1987@gmail.com>
2021-11-06 11:53:51 -0400

    fearutre list add

--- src/task.java
+++ src/task.java
@@ -14,6 +14,20 @@ public class task {
        }
         System.out.println("the length of the number is "+count);
 
-
-
+ArrayList<Integer> arr=new ArrayList<Integer>();
+        arr.add(10);
+        arr.add(15);
+        arr.add(17);
+        arr.add(100);
+        Integer max=arr.get(0);
+        Integer second=arr.get(1);
+        for (Integer a:arr) {
+           if(a>max){
+               max=a;
+           }else if(max>a){
+               second=max;
+            }
+        }
+        System.out.println(max);
+        System.out.println(second);
 }}
